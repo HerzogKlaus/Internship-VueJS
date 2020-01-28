d21ddeb93730dc5e3fbba192f0a94913d8a57961
HerzogKlaus <maxspace2000@mail.ru>
2020-01-28 22:59:00 +0500

    version 1.1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <v-app>
-    <h1 :style="{fontSize: '20px'}" class="mx-5 pa-5">Клиенты этой группы</h1>
   <v-content class="ma-5 pa-1">
     <HelloWorld/>
   </v-content> 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,41 @@
 <template>
     <v-container class="d-flex flex-column">
-    <v-data-table
-      :style="{width: '80%', fontWeight: 'bold'}" 
-      :headers="headers"
-      :items="clients"
-      hide-default-footer>
-      <template v-slot:item.action="{item}">
-        <v-icon
-          small
-          @click="deleteItem(item)"
-        >
+      <h1 class="title">Клиенты этой группы</h1>
+      <v-data-table
+        class="col-lg-10 font-weight-bold col-md-12 col-sm-12"
+        :headers="headers"
+        :items="clients"
+        hide-default-footer>
+          <template v-slot:item.action="{item}">
+            <v-icon
+              small
+              @click="deleteItem(item)"
+            >
           mdi-close
-        </v-icon>
-      </template>
-    </v-data-table>
-    <div class="d-flex flex-row justify-space-between align-center" :style="{width: '80%'}">
-      <div :style="{width: '60%'}">
-    <v-select
-      :items="items"
-      label="Выбрать из списка"
-      outlined
-      :style="{marginTop: '29px'}"
-    ></v-select>
-    </div>
-    <v-btn :style="{fontSize: '12px', textTransform: 'capitalize', height: '55px', justifyContent: 'space-evenly'}" outlined color = 'grey'>
-      Добавить
-    </v-btn>
-    <p class="ma-0" :style="{fontSize: '14px', fontWeight: 'bold'}">или</p>
-    <v-btn text small class="ma-0" :style="{fontSize: '14px', textTransform: 'none', textDecoration: 'underline', fontWeight: 'bold'}">
+            </v-icon>
+          </template>
+      </v-data-table>
+      <div class="add-client d-flex flex-row justify-space-around align-center col-lg-10 flex-wrap">
+        <div class="select-block col-lg-7">
+          <v-select
+            :items="items"
+            label="Выбрать из списка"
+            outlined
+          >
+          </v-select>
+        </div>
+      <v-btn class="mt-n7 add flex-wrap align-center" height="57px" outlined color = 'grey'>
+        Добавить
+      </v-btn>
+      <p class="ma-0 mt-n7 body-2 font-weight-bold">или</p>
+      <v-btn text small class="new-client-create ma-0 mt-n7 font-weight-bold body-2">
       Создать нового клиента
-    </v-btn>
-    </div>
-    <p class="ma-0" :style="{fontWeight: 'bold', fontSize: '14px'}" >Для того, чтобы отредактировать или удалить клиентов из списка, перейдите в раздел <a :style="{textDecoration: 'underline', color: 'inherit'}">Белые списки</a></p>
-    <v-btn :style="{fontSize: '14px', color: '#fff', textTransform: 'none', height: '50px', width: '20%', marginTop: '30px'}" raised color = '#767676'>
-      Сохранить изменения
-    </v-btn>
+      </v-btn>
+      </div>
+      <p class="ma-0 font-weight-bold body-2">Для того, чтобы отредактировать или удалить клиентов из списка, перейдите в раздел <v-btn text class="white-lists pa-0 font-weight-bold">Белые списки</v-btn></p>
+      <v-btn height="50px" class="save-changes font-weight-bold body-2 mt-8 col-lg-3 col-md-12 col-sm-12" raised color = '#767676'>
+        Сохранить изменения
+      </v-btn>
     </v-container>
 </template>
 <script>
@@ -46,6 +47,7 @@
             align: 'left',
             sortable: false,
             value: 'name',
+            
           },
           { text: 'НОМЕР ТЕЛЕФОНА', value: 'phone',sortable: false},
           { text: 'ГРУППА', value: 'group', sortable: false },
